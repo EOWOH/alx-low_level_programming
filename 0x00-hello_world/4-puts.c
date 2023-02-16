a4afa2159fa82606ffc1a82273bdf9c175ceb490
EOWOH <ebukaowoh@gmail.com>
2023-02-16 14:21:47 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry level
+ *
+ * Return: return 0 if successful or 1 if failed
+ */
+
+int main(void)
+{
+	char name[] = "\"Programming is like building a multilingual puzzle";
+	puts(name);
+	return (0);
+}
