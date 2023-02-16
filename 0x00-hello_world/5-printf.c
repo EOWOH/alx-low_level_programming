50cd6abcce4cc441327697e7b8834b9151e2d2a1
EOWOH <ebukaowoh@gmail.com>
2023-02-16 15:17:09 -0800

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry level
+ *
+ * Return: return 0 if successful or 1 if failed
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
