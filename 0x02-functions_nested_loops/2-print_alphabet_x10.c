61ac6bb9a02f5ec26a539577c5709389c5e68b6b
EOWOH <ebukaowoh@gmail.com>
2023-02-23 11:28:24 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,10 +10,10 @@ void print_alphabet_x10(void)
 	char i = 1;
 	char j;
 
-	while(i <= 10)
+	while (i <= 10)
 	{
 		j = 'a';
-		while(j <= 'z')
+		while (j <= 'z')
 		{
 			_putchar(j);
 			j++;
