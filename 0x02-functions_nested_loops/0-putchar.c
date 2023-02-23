9f6bed9378052b51e3021fefa3300e88dd133136
EOWOH <ebukaowoh@gmail.com>
2023-02-23 01:54:52 -0800

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char *str = "_putchar";
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+	return (0);
+}
