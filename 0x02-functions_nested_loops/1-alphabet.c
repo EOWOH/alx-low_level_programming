5f3a838c122d19ba821a4b5b3200c7e92d391ff4
EOWOH <ebukaowoh@gmail.com>
2023-02-23 04:38:49 -0800

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,14 @@
 
 /**
  * main - Entry point
- *
+ * Description a function that prints the alphabet, in lowercase, followed by a new line
  * Return: Always 0 (success)
  */
 void print_alphabet(void)
 {
 	char i;
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
